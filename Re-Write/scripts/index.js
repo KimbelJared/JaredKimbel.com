1497404b245e8dbb0d7a0279e465b099ac783792
Jared <kimbeljared@gmail.com>
2019-02-05 15:59:16 -0800

    Clean Sweep

--- Re-Write/scripts/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * index.js
- * - All our useful JS goes here, awesome!
- */
-
-function contact()
-{
-  confirm("Not Finished Yet, Sorry!")
-}
--- public_html/scripts/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * index.js
- * - All our useful JS goes here, awesome!
- */
-
-function notDone()
-{
-  confirm("Not Finished Yet, Sorry!")
-}
